b539fc5047ef8a769d8207e70110a8d1592242fa
DAKIRDYA <dakirdya@yandex.ru>
2024-12-08 17:36:40 +0700

    3.13. На связи база данных

--- project/apps/publications/src/main.ts
+++ project/apps/publications/src/main.ts
@@ -6,12 +6,21 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Publications app')
+    .setDescription('The publications API description')
+    .setVersion('1.0')
+    .addTag('publications')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
